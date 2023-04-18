ed981271a3fb1aed629ca129f01382bec109a507
Victoria Kirichenko <vakirichenko02@gmail.com>
2023-04-18 11:19:41 +0300

    editing main of flask part

--- flask_project/main.py
+++ flask_project/main.py
@@ -1,21 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
-# from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-from datetime import datetime, timedelta
-from random import randint
-import os
-import pandas as pd
-import psycopg2
+from flask import Flask, render_template, request, url_for, redirect
 import ast
-
+from config import get_db_connection
 
 app = Flask(__name__)
-
-def get_db_connection():
-    conn = psycopg2.connect(host='84.201.175.11',
-                            database='wishlist',
-                            user='web_app',
-                            password='shopsdb9274')
-    return conn
 
 
 def get_info_by_id(product_id,cur):
@@ -35,36 +22,27 @@ def get_info_by_id(product_id,cur):
 
 @app.route('/')
 def index():
-    # сделать url_for на products
-    return render_template('index.html')
+    return redirect(url_for('products'))
+    # return render_template('index.html')
 
 @app.route('/products/', methods=['GET', 'POST'])
 def products():
     if request.method == 'POST':
         keys=[]
         for key in request.form:
-            # print(key, request.form[key])
             keys.append((key, request.form[key]))
         ids = keys[::2]
         options = keys[1::2]
-        # print(ids)
-        # print(options)
-        
-        # option = request.form.getlist('options')
-        # print(request.form.get('match0'))
-        # print(request.form.get('match1'))
-        # тут надо еще проверить, что все элементы не None
+
         conn = get_db_connection()
         cur = conn.cursor()
         for id, option in zip(ids,options):
             id_ =  request.form.get(id[0])
             option_ = request.form.get(option[0])
-            # print('id',id_,'match', option_)
             
             cur.execute('UPDATE pairs SET match_type = %s WHERE pair_id = %s;',(str(option_),int(id_)))
             updated_rows = cur.rowcount
             conn.commit()
-            # print(updated_rows)
         cur.close()
         conn.close()
         
@@ -72,9 +50,9 @@ def products():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT pair_id, id_1, id_2 FROM pairs WHERE match_type = (%s) LIMIT 4;',('-1',))
+    # cur.execute('SELECT pair_id, id_1, id_2 FROM pairs_by_model WHERE match_type > 4 LIMIT 4;')
     pairs = cur.fetchall()
 
-    # print(pairs,cur)
     pairs_to_show = []
     for pair in pairs:
         product1 = get_info_by_id(pair[1],cur)
@@ -85,34 +63,9 @@ def products():
             'product2': product2
             }
         pairs_to_show.append(new_pair)
-        
-
 
     cur.close()
     conn.close()
-
-    # print(pairs_to_show[0])
-    # print(len(pairs_to_show))
-
-
-
-
-
-    data = []
-    pair1 = {
-        'id': 0,
-        'title_1':'смартфон1',
-        'brand_1':'brand1',
-        'price_1':'price1',
-        'description_1':'desc1',
-        'attributes_1': 'attrs',
-        'title_2':'смартфон2',
-        'brand_2':'brand2',
-        'price_2':'price2',
-        'description_2':'desc2',
-        'attributes_2': 'attr2'
-    }
-    data.append(pair1)
 
     return render_template('products1.html',data = pairs_to_show, displays = [0,0,0,0])
 
